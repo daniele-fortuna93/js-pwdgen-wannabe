e34e3af2700d2f2f0f4b7de64126e9fc14a74aa1
Daniele <bambudio@libero.it>
2021-02-25 15:42:17 +0100

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// metodo 1
+var nome = prompt("Inserisci il tuo nome");
+var cognome = prompt("Inserisci il tuo cognome");
+var colore = prompt("Inserisci il tuo colore");
+document.getElementById('pass').innerHTML = nome + cognome + colore + 27;
+
+// metodo 2
+// var name = prompt("Inserisci il tuo nome");
+// var surname = prompt("Inserisci il tuo cognome");
+// var color = prompt("Inserisci il tuo colore");
+// var pasS = name + surname + color + 27;
+// document.getElementById('pass').innerHTML = pasS;
+
+// metodo 3
+// var pass = prompt("Inserisci il tuo nome") + prompt("Inserisci il tuo cognome") + prompt("Inserisci il tuo colore") + 27;
+// document.getElementById('pass').innerHTML = pass;
